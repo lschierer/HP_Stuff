3dd8adc5adef25e7df4a7b8cfeeedf5b7e86da99
Luke Schierer <lschiere@amazon.com>
2024-12-17 16:47:13 -0500

    starting to migrate to Astro v5 content collections

--- starlight/src/content.config.ts
+++ starlight/src/content.config.ts
@@ -1,7 +1,22 @@
-import { defineCollection } from 'astro:content';
-import { docsLoader } from '@astrojs/starlight/loaders';
-import { docsSchema } from '@astrojs/starlight/schema';
+import { docsSchema } from "@astrojs/starlight/schema";
+
+import { type SchemaContext, z, defineCollection } from "astro:content";
+import { docsLoader } from "@astrojs/starlight/loaders";
+
+import { Database } from "@lib/GrampsZodTypes";
+
+import { history, event } from "@schemas/index";
+
+const HeroSchema = (ctx: SchemaContext) => docsSchema()(ctx).shape.hero;
+
+const extendedDocs = (ctx: SchemaContext) =>
+  docsSchema()(ctx).extend({
+    hero: z.union([z.string(), HeroSchema(ctx)]).optional(),
+  });
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  docs: defineCollection({
+    loader: docsLoader(),
+    schema: extendedDocs,
+  }),
 };
