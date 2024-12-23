6df6370da7f87aaa9e94854a3b030a61ed583be9
Luke Schierer <luke@schierer.org>
2024-12-22 19:59:40 -0500

    no longer needed for reference, replaced with native astro components

--- starlight/src/components/grampsParser/config.ts
+++
@@ -1,6 +0,0 @@
-export const prerender = false;
-//if I define file as a property, then I have to set it each time
-//the component is used.  That might work for this one, but is
-//going to be a pain for individuals and families because astro
-//unlike hugo, has no frontmatter cascade.
-export const file = "potter_universe.gramps";
