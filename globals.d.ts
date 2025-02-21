113d60827bb358e5118019c01fac6dad9ab00d7b
Luke Schierer <lschiere@amazon.com>
2025-02-21 12:26:17 -0500

    build: type safety for the pure html web components
    this needs to be tested with the lit components

--- globals.d.ts
+++ globals.d.ts
@@ -1,4 +1,7 @@
 // ./src/globals.d.ts
 
 // Recognize all CSS files as module imports.
-declare module "*.css" {}
+declare module "*.css" {
+  const stylesheet: CSSStyleSheet;
+  export default stylesheet;
+}
