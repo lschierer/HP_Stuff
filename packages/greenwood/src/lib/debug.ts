8212e103ece16a44ce27f710e50a9d770374f227
Luke Schierer <lschiere@amazon.com>
2025-02-21 19:29:24 -0500

    build: debug file paths
    I noticed some files are sending fully qualified paths and not relative
    paths to the debug function, which of course could not then match them.
    
    This is an attempt to handle it.  However, it is failing, or rather it
    partially works, but causes CORS errors that prevent the layout from
    working correctly.

--- packages/greenwood/src/lib/debug.ts
+++ packages/greenwood/src/lib/debug.ts
@@ -1,12 +1,20 @@
+import path from "node:path";
+import process from "node:process";
+
 const fileDebug: Record<string, boolean> = {
   "/lib/debug.ts": false,
+  "/lib/BookmarksList.ts": true,
   "/components/DirectoryIndex.ts": false,
   "/components/theme.ts": false,
   "/components/SiteTitle.ts": false,
   "/components/ThemeSelector.ts": false,
+  "/pages/Bookmarks/ResponsibleAdults.ts": true,
 };
 
 const debugFunction = (myName: string): boolean => {
+  if (path.isAbsolute(myName)) {
+    myName = `/${path.relative(`${process.cwd()}/src`, myName)}`;
+  }
   return fileDebug[myName];
 };
 
