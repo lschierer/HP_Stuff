be403504a25fababcc366f5aff31d1570af1a593
Luke Schierer <luke@schierer.org>
2025-02-23 00:28:34 -0500

    build: disable debugging

--- packages/greenwood/src/lib/debug.ts
+++ packages/greenwood/src/lib/debug.ts
@@ -5,12 +5,12 @@ if (DEBUG) {
 }
 const fileDebug: Record<string, boolean> = {
   "/lib/debug.ts": false,
-  "/lib/BookmarksList.ts": true,
+  "/lib/BookmarksList.ts": false,
   "/components/DirectoryIndex.ts": false,
   "/components/theme.ts": false,
   "/components/SiteTitle.ts": false,
   "/components/ThemeSelector.ts": false,
-  "/pages/Bookmarks/ResponsibleAdults.ts": true,
+  "/pages/Bookmarks/ResponsibleAdults.ts": false,
 };
 
 function isAbsolutePath(path: string): boolean {
