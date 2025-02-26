bcd7d296e0cec5e3b7524a168b5e56be2c557f4b
Luke Schierer <lschiere@amazon.com>
2025-02-26 12:57:42 -0500

    feat: recreate bookmarks pages
    another category moved, add one story

---
+++ packages/greenwood/src/pages/Bookmarks/Problems Exposed.ts
@@ -0,0 +1,63 @@
+import { type Compilation, type Route } from "../../lib/greenwoodPages.ts";
+import "../../lib/BookmarksList.ts";
+import BookmarksList from "../../lib/BookmarksList.ts";
+
+import markdownTextProcessing from "../../lib/customMarkdownProcessing.ts";
+
+import debugFunction from "../../lib/debug.ts";
+const DEBUG = debugFunction(new URL(import.meta.url).pathname);
+if (DEBUG) {
+  console.log(`DEBUG enabled for ${new URL(import.meta.url).pathname}`);
+}
+
+async function getBody() {
+  const bodyText = `
+While The Harry Potter universe offers a great foundation for building on, it also has plenty
+of places where Mrs. Rowling has left us with inconsistencies, contradictions, and other plot
+holes. These stories, often through the use of satire and/or farce, expose some of those.
+`;
+  const bookmarksList = new BookmarksList();
+  bookmarksList.category = "Problems Exposed";
+  await bookmarksList.ParseBookmarks().then(() => {
+    if (DEBUG) {
+      console.log(
+        `after parsing getBody sees ${bookmarksList.bookmarks.length} bookmarks`
+      );
+    }
+  });
+  return markdownTextProcessing(bodyText).concat(`
+    <dl>
+      ${bookmarksList.listBookMarks()}
+    </dl>
+    `);
+}
+
+function getFrontmatter() {
+  return {
+    title: "Exposing Plot Holes",
+    collection: "Bookmarks",
+    description: "HP stories HP stories that expose plot holes in the original",
+    author: "Luke Schierer",
+  };
+}
+
+function getLayout(compilation: Compilation, route: Route) {
+  return `
+  <body>
+    <header>
+      <h1 class="spectrum-Heading spectrum-Heading--sizeXXL">
+        ${route.title ? route.title : route.label}
+      </h1>
+      <link rel="stylesheet" href="/styles/BookmarksList.css" />
+    </header>
+
+    <div class="main">
+      <div class="content">
+        <content-outlet></content-outlet>
+
+      </div>
+    </div>
+  </body>
+  `;
+}
+export { getFrontmatter, getBody, getLayout };
