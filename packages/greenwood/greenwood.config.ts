ec0cea0e231117fb2daa0d55a452961cf98847d6
Luke Schierer <luke@schierer.org>
2025-04-03 01:36:10 -0400

    fix: proper config file loading

--- packages/greenwood/greenwood.config.ts
+++ packages/greenwood/greenwood.config.ts
@@ -1,15 +1,18 @@
 import { greenwoodPluginPostCss } from "@greenwood/plugin-postcss";
+import type { Config } from "@greenwood/cli";
 
 import { greenwoodSpectrumThemePack } from "greenwoodspectrumtheme";
 
 import { GedcomPeopleSourcePlugin } from "./src/plugins/gramps/people.ts";
 import { GedcomFamilySourcePlugin } from "./src/plugins/gramps/families.ts";
 
-import { loadConfig, type Config as PackConfig } from "./src/lib/config.ts";
+import {
+  loadConfig,
+  type Config as PackConfig,
+} from "greenwoodspectrumtheme/config";
 import localConfig from "./src/spectrum-theme.config.ts";
 
 const config = loadConfig(localConfig) as PackConfig;
-
 
 const gc: Config = {
   useTsc: true,
