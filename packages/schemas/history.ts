3c4f4e382a02a9c5ce13ff60c0f8d3f679b88cc7
Luke Schierer <lschiere@amazon.com>
2025-04-17 18:43:34 -0400

    build: current state

--- packages/schemas/history.ts
+++ packages/schemas/history.ts
@@ -1,9 +1,23 @@
 import { z } from "zod";
 
-import { event } from "./event";
-
-export const history = z.object({
-  events: z.union([event, z.array(event)]),
+export const HistoryEvent = z.object({
+  type: z.string(),
+  blurb: z.string(),
+  description: z.string().optional(),
+  date: z.union([z.date(), z.number(), z.string()]).optional(),
+  source: z.string().nullish(),
 });
 
-export type history = z.infer<typeof history>;
+export type HistoryEvent = z.infer<typeof HistoryEvent>;
+
+export const History = z.object({
+  events: z.union([HistoryEvent, z.array(HistoryEvent)]),
+});
+
+export type History = z.infer<typeof History>;
+
+export const DisplayableEvent = HistoryEvent.extend({
+  date: z.union([z.date(), z.literal("unknown date")]),
+});
+
+export type DisplayableEvent = z.infer<typeof DisplayableEvent>;
--- packages/schemas/index.ts
+++ packages/schemas/index.ts
@@ -1,7 +1,7 @@
-export { event } from "./event";
-export { history } from "./history";
+export { HistoryEvent, History, DisplayableEvent } from "./history";
 export { FrontMatter } from "./page";
 export { ParsedResult } from "./page";
 export { ExternalPage } from "./page";
 export { NavigationItem } from "./page";
 export { Config } from "./config";
+export { Bookmark } from "./bookmarks";
