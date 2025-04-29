3675e0d941703a68351188b265531683295928b5
Luke Schierer <luke@schierer.org>
2025-04-29 00:06:08 -0400

    fix: random small fixes
    the sst build always fails if I try to do both builds at once, let just handle them separately
    update mise managed tools
    fix a few types in the Anti-Dumbledore file

--- packages/greenwood/src/pages/Bookmarks/Anti-Dumbledore.ts
+++ packages/greenwood/src/pages/Bookmarks/Anti-Dumbledore.ts
@@ -1,5 +1,10 @@
 export const prerender = true; // reduce the number of pages that the router needs to handle.
-import { type GetFrontmatter, type GetBody } from "@greenwood/cli";
+
+import {
+  type Compilation,
+  type Page,
+  type GetFrontmatter,
+} from "@greenwood/cli";
 import "../../lib/BookmarksList.ts";
 import BookmarksList from "../../lib/BookmarksList.ts";
 
@@ -10,25 +15,22 @@ const DEBUG = debugFunction(new URL(import.meta.url).pathname);
 if (DEBUG) {
   console.log(`DEBUG enabled for ${new URL(import.meta.url).pathname}`);
 }
-
 import bookmarksData from "@hp-stuff/assets/dist/Bookmarks/Anti-Dumbledore.json" with { type: "json" };
 
-const getBody: GetBody = async () => {
-  /*start work around for GetFrontmatter requiring async */
-  await new Promise((resolve) => setTimeout(resolve, 1));
-  /* end workaround */
-
+const getBody: (
+  compilation: Compilation,
+  page: Page,
+  request: Request
+) => string | Promise<string> = () => {
   const bodyText = `
 These stories are reactions to the way that Mrs. Rowling essentially justified every bad decision that [Dumbledore] made by having [Harry] not only forgive him, but name a child after him.
 
 [Dumbledore]: </Harrypedia/people/Dumbledore/Albus Percival Wulfric Brian/>
 [Harry]: </Harrypedia/people/Potter/Harry James/>
 `;
-  const dataArray = bookmarksData;
 
   const bookmarksList = new BookmarksList();
-  bookmarksList.ParseBookmarks(dataArray);
-
+  bookmarksList.ParseBookmarks(bookmarksData);
   return markdownTextProcessing(bodyText).concat(`
     <dl>
       ${bookmarksList.listBookMarks()}
