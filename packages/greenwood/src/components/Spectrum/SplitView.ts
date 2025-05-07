c8763d116e8cd96a9289fff2fd1b4b674c9425f3
Luke Schierer <lschiere@amazon.com>
2025-05-07 17:57:36 -0400

    fix: these should not have the node_modules

--- packages/greenwood/src/components/Spectrum/SplitView.ts
+++ packages/greenwood/src/components/Spectrum/SplitView.ts
@@ -1,5 +1,5 @@
 import "@spectrum-web-components/split-view/sp-split-view.js";
-import SideBarCSS from "/node_modules/@hp-stuff/assets/dist/styles/sidebar.css" with { type: "css" };
+import SideBarCSS from "@hp-stuff/assets/dist/styles/sidebar.css" with { type: "css" };
 if (document.querySelector(".spectrum-SideNav")) {
   document.adoptedStyleSheets = [...document.adoptedStyleSheets, SideBarCSS];
 }
